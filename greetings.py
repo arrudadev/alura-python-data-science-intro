0a512e8bdebe6020a4a5940609d62645f93aa58e
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-02-26 14:53:04 -0300

    rename main.py to greetings.py

---
+++ greetings.py
@@ -0,0 +1,9 @@
+def greetings():
+  first_name = input('Qual é seu primeiro nome? ')
+  last_name = input('Qual é seu sobrenome? ')
+
+  full_name = first_name + ' ' + last_name
+
+  print(f'Olá {full_name}')
+
+greetings()
--- main.py
+++
@@ -1,9 +0,0 @@
-def full_name():
-  first_name = input('Qual é seu primeiro nome? ')
-  last_name = input('Qual é seu sobrenome? ')
-
-  my_full_name = first_name + ' ' + last_name
-
-  print(f'Olá {my_full_name}')
-
-full_name()
