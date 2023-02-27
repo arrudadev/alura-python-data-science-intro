68c1b386141c46783813f43db918a7377fac88e2
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-02-27 13:19:33 -0300

    change student grades to plot graph with matplotlib

--- student-grades.py
+++ student-grades.py
@@ -1,13 +1,25 @@
+import matplotlib.pyplot as plt
 from random import randrange, seed
 
 seed(11)
 
-randrange(0,11)
+randrange(0, 11)
 
 math_grades = []
 
 for math_grade in range(8):
-  math_grades.append(randrange(0,11))
+  math_grades.append(randrange(0, 11))
 
-print('math grades: ', math_grades)
-print('length: ', len(math_grades))
+x = list(range(1, 9))
+y = math_grades
+
+print(x)
+print(y)
+
+plt.plot(x, y, marker='o')
+plt.title('Notas de matemática')
+plt.xlabel('Provas')
+plt.ylabel('Notas')
+
+# plt.show()
+plt.savefig("mygraph.png")
